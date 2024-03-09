02e9c1507a07dab3a27ad5f0f1d9f3af2c3d1b72
Ilya <ilya.gu4ick@gmail.com>
2024-03-09 17:22:21 +0300

    fix

--- src/compenents/modal/Modal.tsx
+++ src/compenents/modal/Modal.tsx
@@ -1,35 +1,32 @@
-"use client";
-import { FC, useEffect, useRef } from "react";
-import { TypePropsModal } from "./Modal.type";
-import styles from "./Modal.module.scss";
+'use client'
+import { FC, useEffect, useRef } from 'react'
+import { TypePropsModal } from './Modal.type'
+import styles from './Modal.module.scss'
+import clsx from 'clsx'
 const Modal: FC<TypePropsModal> = ({ showModal, setShowModal, content }) => {
-  useEffect(() => {
-    const handleClick = (event: any) => {
-      const path = event.path || (event.composedPath && event.composedPath());
-      if (path.includes(modalRef.current)) {
-        setShowModal(true);
-      } else {
-        setShowModal(false);
-      }
-    };
-    document.body.addEventListener("click", handleClick);
+	useEffect(() => {
+		const handleClick = (event: any) => {
+			const path = event.path || (event.composedPath && event.composedPath())
+			if (path.includes(modalRef.current)) {
+				setShowModal(true)
+			} else {
+				setShowModal(false)
+			}
+		}
+		document.body.addEventListener('click', handleClick)
 
-    return () => {
-      document.body.addEventListener("click", handleClick);
-    };
-  }, []);
-  const modalRef = useRef(null);
-  return (
-    <>
-      {showModal ? (
-        <>
-          <div className={styles.wrapper}>
-            <div ref={modalRef}>{content}</div>
-          </div>
-        </>
-      ) : null}
-    </>
-  );
-};
+		return () => {
+			document.body.addEventListener('click', handleClick)
+		}
+	}, [])
+	const modalRef = useRef(null)
+	return (
+		<>
+			<div className={clsx(styles.wrapper, !showModal && styles.hidden)}>
+				<div ref={modalRef}>{content}</div>
+			</div>
+		</>
+	)
+}
 
-export default Modal;
+export default Modal
